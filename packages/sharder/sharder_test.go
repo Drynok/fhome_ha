86d66a2d3885bd695cfa01f8cbd079f44a21267b
Tim Abdullin <timerhali.abdullin@eleks.com>
2023-08-23 09:37:04 +0300

    Added draft unit tests

--- packages/sharder/sharder_test.go
+++ packages/sharder/sharder_test.go
@@ -1,27 +1,51 @@
 package sharder_test
 
-// func TestShardJSON(t *testing.T) {
-// 	inputJSON := `["value1", "value2", "value3", "value4", "value5"]`
-// 	numShards := 3
+import (
+	"testing"
 
-// 	expectedShards := []Shard{
-// 		{ID: 0, Values: []string{"value1", "value4"}},
-// 		{ID: 1, Values: []string{"value2", "value5"}},
-// 		{ID: 2, Values: []string{"value3"}},
-// 	}
+	"github.com/Drynok/fhome_ha/packages/sharder"
+)
 
-// 	shards, err := ShardJSON(inputJSON, numShards)
-// 	if err != nil {
-// 		t.Errorf("Unexpected error: %v", err)
-// 	}
+func TestShard(t *testing.T) {
+	list := []int{1, 2, 3, 4, 5}
+	numShards := 3
 
-// 	if len(shards) != len(expectedShards) {
-// 		t.Errorf("Expected %d shards, but got %d", len(expectedShards), len(shards))
-// 	}
+	shards, err := sharder.Shard(list, numShards)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
 
-// 	for i := range shards {
-// 		if !reflect.DeepEqual(shards[i], expectedShards[i]) {
-// 			t.Errorf("Expected shard %+v, but got %+v", expectedShards[i], shards[i])
-// 		}
-// 	}
-// }
+	expectedShards := []sharder.ShardItem[int]{
+		{ID: 0, Values: []int{1, 4}},
+		{ID: 1, Values: []int{2, 5}},
+		{ID: 2, Values: []int{3}},
+	}
+
+	if len(shards) != len(expectedShards) {
+		t.Errorf("expected %d shards but got %d", len(expectedShards), len(shards))
+	}
+
+	for i := range shards {
+		if shards[i].ID != expectedShards[i].ID {
+			t.Errorf("expected shard ID %d but got %d", expectedShards[i].ID, shards[i].ID)
+		}
+
+		if !equalValues(shards[i].Values, expectedShards[i].Values) {
+			t.Errorf("expected shard values %v but got %v", expectedShards[i].Values, shards[i].Values)
+		}
+	}
+}
+
+func equalValues(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
